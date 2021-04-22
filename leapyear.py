287c3a48ede28bd00e39f8027107f1fee4953fe7
Casey Nord <nordcw@gmail.com>
2021-04-22 16:54:56 -0700

    leap year code with error handling

--- leapyear.py
+++ leapyear.py
@@ -3,7 +3,15 @@
 # spring 2021
 
 # get user input from console
-year = int(input("Enter year as a positive integer: "))
+while True:
+    try:
+        year = int(input("Enter year as a positive integer: "))
+        if year < 0:
+            raise ValueError
+
+        break  # exit loop on valid input
+    except ValueError:
+        print("Invalid input, please try again...")
 
 # test value for leap year condition
 valid_condition_01 = (year % 4 == 0) and (year % 100 != 0)
